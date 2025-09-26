79803f82ad8419fd29d1bd6343689c67b8a9dc23
BrunoAlexys <brunoalexysmoura@gmail.com>
2025-09-26 10:28:25 -0300

    first commit

---
+++ src/main/java/br/com/apicomanda/ApiComandaApplication.java
@@ -0,0 +1,13 @@
+package br.com.apicomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apicomanda/ApiComandaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apicomanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
